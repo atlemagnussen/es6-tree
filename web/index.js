25aec7f819251cefa24ee117e4f3cd42ef9ea314
Atle Magnussen <atlemagnussen@gmail.com>
2018-05-23 23:39:54 +0200

    test with octicons

--- web/index.js
+++ web/index.js
@@ -1,9 +1,33 @@
 import { EzTree } from './eztree.js';
-const eztree = new EzTree();
+
 
 class Index {
     constructor() {
         console.log("Index init");
+
+        var nodes = [{
+            id: "master",
+            name: "#",
+            expanded: true,
+            children: [
+                {
+                    id: "web-easy",
+                    name: "Web Easy"
+                },
+                {
+                    id: "web-medium",
+                    name: "Web Medium"
+                }
+            ]
+        }];
+
+        var config = {
+            types: {
+                main: {
+                    css: "icon octicon-pound"
+                }
+            }
+        }
     }
 }
 
