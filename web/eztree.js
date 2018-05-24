d0fccbb1b58fb7258a88450cb70b870b3484f47e
Atle Magnussen <atlemagnussen@gmail.com>
2018-05-24 22:53:58 +0200

    made simple tree js lib

--- web/eztree.js
+++ web/eztree.js
@@ -1,5 +1,27 @@
 export class EzTree {
-    constructor() {
+    constructor(parentId, config, data) {
         console.log('ez-tree');
+        this.parentId = parentId;
+        this.parentEl = document.getElementById(parentId);
+        if (!this.parentEl) {
+            throw new Error(`Can't find element with id ${parentId}`);
+        }
+        this.config = config;
+        this.data = data;
+        this.append(this.parentEl, this.data);
+    }
+
+    append(p, data) {
+        data.forEach(n => {
+            var d = document.createElement('details');
+            p.appendChild(d);
+            var s = document.createElement('summary');
+            s.id = n.id;
+            s.innerHTML = n.name;
+            d.appendChild(s);
+            if (n.children && Array.isArray(n.children)) {
+                this.append(d, n.children);
+            }
+        })
     }
 }
--- web/index.js
+++ web/index.js
@@ -1,25 +1,50 @@
-import { EzTree } from './eztree.js';
+import {
+    EzTree
+} from './eztree.js';
 
 
 class Index {
     constructor() {
         console.log("Index init");
 
-        var nodes = [{
-            id: "master",
-            name: "#",
-            expanded: true,
-            children: [
-                {
-                    id: "web-easy",
-                    name: "Web Easy"
-                },
-                {
-                    id: "web-medium",
-                    name: "Web Medium"
-                }
-            ]
-        }];
+        var data = [{
+                id: "master",
+                name: "#",
+                expanded: true,
+                children: [{
+                        id: "web-easy",
+                        name: "Web Easy",
+                        children: [{
+                                id: "js-beginners",
+                                name: "JS beginners"
+                            },
+                            {
+                                id: "html-beginners",
+                                name: "HTML beginners"
+                            }
+                        ]
+                    },
+                    {
+                        id: "web-medium",
+                        name: "Web Medium"
+                    }
+                ]
+            },
+            {
+                id: "master2",
+                name: "#2",
+                expanded: true,
+                children: [{
+                        id: "web-easy2",
+                        name: "Web Easy2"
+                    },
+                    {
+                        id: "web-medium2",
+                        name: "Web Medium2"
+                    }
+                ]
+            }
+        ];
 
         var config = {
             types: {
@@ -28,6 +53,8 @@ class Index {
                 }
             }
         }
+
+        this.tree = new EzTree('eztree', config, data)
     }
 }
 
