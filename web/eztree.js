51d75fc154b11d683d475d65c6d556bb73b39dca
Atle Magnussen <atlemagnussen@gmail.com>
2018-05-24 23:26:09 +0200

    added more stuff to the tree

--- web/eztree.js
+++ web/eztree.js
@@ -6,6 +6,7 @@ export class EzTree {
         if (!this.parentEl) {
             throw new Error(`Can't find element with id ${parentId}`);
         }
+        this.parentEl.classList.add("ez-tree")
         this.config = config;
         this.data = data;
         this.append(this.parentEl, this.data);
@@ -14,14 +15,34 @@ export class EzTree {
     append(p, data) {
         data.forEach(n => {
             var d = document.createElement('details');
+            if (n.expanded) {
+                d.open = true;
+            }
             p.appendChild(d);
             var s = document.createElement('summary');
             s.id = n.id;
-            s.innerHTML = n.name;
+            var span = document.createElement('span')
+            span.innerText = n.name;
+            s.appendChild(span);
+            this.handleType(n, span);
             d.appendChild(s);
             if (n.children && Array.isArray(n.children)) {
                 this.append(d, n.children);
+            } else {
+                d.classList.add("leaf");
             }
         })
+    }
+
+    handleType(node, el) {
+        if (node.type && this.config.types) {
+            if (this.config.types.hasOwnProperty(node.type)) {
+                let type = this.config.types[node.type];
+                if (type.css) {
+                    let classes = type.css.split(' ');
+                    el.classList.add(...classes);
+                }
+            }
+        }
     }
 }
--- web/index.js
+++ web/index.js
@@ -10,23 +10,29 @@ class Index {
         var data = [{
                 id: "master",
                 name: "#",
+                type: "main",
                 expanded: true,
                 children: [{
                         id: "web-easy",
+                        expanded: true,
+                        type: "folder",
                         name: "Web Easy",
                         children: [{
                                 id: "js-beginners",
-                                name: "JS beginners"
+                                name: "JS beginners",
+                                type: "file"
                             },
                             {
                                 id: "html-beginners",
-                                name: "HTML beginners"
+                                name: "HTML beginners",
+                                type: "file"
                             }
                         ]
                     },
                     {
                         id: "web-medium",
-                        name: "Web Medium"
+                        name: "Web Medium",
+                        type: "folder"
                     }
                 ]
             },
@@ -34,13 +40,16 @@ class Index {
                 id: "master2",
                 name: "#2",
                 expanded: true,
+                type: "main",
                 children: [{
                         id: "web-easy2",
-                        name: "Web Easy2"
+                        name: "Web Easy2",
+                        type: "file"
                     },
                     {
                         id: "web-medium2",
-                        name: "Web Medium2"
+                        name: "Web Medium2",
+                        type: "file"
                     }
                 ]
             }
@@ -48,8 +57,11 @@ class Index {
 
         var config = {
             types: {
-                main: {
-                    css: "icon octicon-pound"
+                folder: {
+                    css: "icon icon-file-directory"
+                },
+                file: {
+                    css: "icon icon-file"
                 }
             }
         }
